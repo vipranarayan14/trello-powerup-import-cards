4073264df5abf3e72c94d96642c661b2bcec10a6
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2022-01-17 21:29:06 +0530

    feat: (test) add import modal

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 const { TrelloPowerUp } = window;
 
 const onBoardButtonClick = (t, opts) => {
-  alert("Import Cards");
+  alert("Board button clicked");
+
+  return t.modal({
+    // the url to load for the iframe
+    url: new URL("import.html", import.meta.url),
+    // whether the modal should stretch to take up the whole screen
+    fullscreen: true,
+    // optional function to be called if user closes modal (via `X` or escape, etc)
+    callback: () => console.log("Goodbye."),
+    // optional title for header chrome
+    title: "Import cards",
+    // optional action buttons for header chrome
+    // max 3, up to 1 on right side
+    actions: [
+      {
+        icon: new URL("icon.png", import.meta.url),
+        url: "https://google.com",
+        alt: "Leftmost",
+        position: "left",
+      },
+    ],
+  });
 };
 
 TrelloPowerUp.initialize({
